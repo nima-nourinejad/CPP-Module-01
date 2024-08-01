636d5fd14586cabd82ce92d5d1a5212006bd0591
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-01 11:13:46 +0300

    ex06

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,10 +1,38 @@
 #include "Harl.hpp"
 
-int main(void) {
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr
+        << "Input one of the following levels: DEBUG, INFO, WARNING, or ERROR"
+        << std::endl;
+    return 1;
+  }
   Harl harl;
-  harl.complain("DEBUG");
-  harl.complain("INFO");
-  harl.complain("WARNING");
-  harl.complain("ERROR");
+  std::string level = argv[1];
+  std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+  int index;
+  for (index = 0; index < 4; index++) {
+    if (level == levels[index])
+      break;
+  }
+  switch (index) {
+  case 0:
+    harl.complain("DEBUG");
+    // fallthrough
+  case 1:
+    harl.complain("INFO");
+    // fallthrough
+  case 2:
+    harl.complain("WARNING");
+    // fallthrough
+  case 3:
+    harl.complain("ERROR");
+    break;
+  default:
+    std::cout
+        << "The level you entered is not one of the following: DEBUG, INFO, "
+           "WARNING, or ERROR"
+        << std::endl;
+  }
   return 0;
 }
