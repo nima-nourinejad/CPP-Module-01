24ec5bbd01409d6cc5bcfba24b489a57dfc9b79c
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-01 08:52:39 +0300

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,22 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+
+// void test(void) {
+//   std::string fileName = "test.txt";
+//   std::string content = "ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF"
+//                         "ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF"
+//                         "ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF";
+//   std::ofstream file(fileName.c_str());
+//   file << content;
+//   file.close();
+// }
+// #include <cstdio>
+// void removeFiles(void) {
+//   std::remove("test.txt");
+//   std::remove("test.txt.replace");
+// }
 
 int main(int argc, char **argv) {
   if (argc != 4) {
@@ -12,5 +28,39 @@ int main(int argc, char **argv) {
   std::string fileName = argv[1];
   std::string s1 = argv[2];
   std::string s2 = argv[3];
-  std::ofstream logFile(fileName, std::ios::app);
+  if (fileName.empty() || s1.empty()) {
+    std::cerr << "File Name and S1 can not be empty." << std::endl;
+    return 1;
+  }
+  std::ifstream file(fileName.c_str());
+  if (!file.is_open()) {
+    std::cerr << "File does not exist or cannot be opened." << std::endl;
+    return 1;
+  }
+  std::stringstream read;
+  read << file.rdbuf();
+  file.close();
+  std::string content = read.str();
+  std::stringstream result;
+  std::string::size_type start = 0;
+  std::string::size_type find = 0;
+  while (true) {
+    find = content.find(s1, start);
+    if (find == std::string::npos) {
+      result << content.substr(start);
+      break;
+    }
+    result << content.substr(start, find - start) << s2;
+    start = find + std::string(s2).length();
+  }
+  std::stringstream replaceName;
+  replaceName << fileName << ".replace";
+  std::ofstream replaceFile(replaceName.str().c_str());
+  if (!replaceFile.is_open()) {
+    std::cerr << "Could not create the replace file." << std::endl;
+    return 1;
+  }
+  replaceFile << result.str();
+  replaceFile.close();
+  return 0;
 }
---
+++ ex04/test.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+// #include <iostream>
+// #include <sstream>
+#include <string>
+
+int main(void) {
+  std::string fileName = "test.txt";
+  std::string content = "ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF"
+                        "ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF"
+                        "ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF";
+  std::ofstream file(fileName.c_str());
+  file << content;
+  file.close();
+  return 0;
+}
